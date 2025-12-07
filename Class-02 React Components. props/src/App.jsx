cee3c719704bb04d38501c6210e96d550f1626e2
class12 <mansoorihassan409@gmail.com>
2025-12-07 18:10:39 +0500

    React Components props

---
+++ Class-02 React Components. props/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/Button'
+function App() {
+  return (
+    <>
+      <div className='text-red-500'>Hello World</div>
+      <div className='text-red-500'>Hello World</div>
+      <Button />
+    </>
+  )
+}
+
+export default App
