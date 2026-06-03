2c8e04666d8e9acd9f7e3833650824dcc739d8e7
class12 <mansoorihassan409@gmail.com>
2026-06-03 10:37:30 +0500

    practice react-router-dom

---
+++ Class-16 React Router DOM Practice/src/Navigator.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+
+
+
+const Navigator = () => {
+    return(
+        <>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+             <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        </BrowserRouter>
+        
+        </>
+    )
+}
+
+export default Navigator;
